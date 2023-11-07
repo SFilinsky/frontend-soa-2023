2c0f594d26c85393e770a7d5edfcd02c8167e7a4
Siarhei Filinski <sfilisnky@gmail.com>
2023-11-07 18:40:54 +0100

    Cleaned Container module.

--- packages/teammate-search/container/next.config.js
+++ packages/teammate-search/container/next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+const getRemotes = (isServer) => {
+    const location = isServer ? 'ssr' : 'chunks';
+    return {
+        // specify remotes
+        auth: `auth@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    };
+}
+
+module.exports = {
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        const { isServer } = options;
+
+        config.plugins.push(
+            new NextFederationPlugin({
+                name: "container",
+                filename: "static/runtime/app2remoteEntry.js",
+                remotes: getRemotes(isServer),
+                exposes: {
+
+                },
+                shared: [ 'react-dom' ]
+            }),
+        )
+
+        return config;
+    }
+}
--- packages/teammate-search/container/src/app/layout.tsx
+++ packages/teammate-search/container/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import React from "react";
+import IntegrateModule from "@/app/IntegrateModule";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header> header </header>
+        <IntegrateModule mount={() => {}}></IntegrateModule>
+        {children}
+      </body>
     </html>
   )
 }
