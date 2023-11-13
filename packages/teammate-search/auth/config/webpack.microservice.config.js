530e99b34379e1cc4dc96f015972209093645dd9
Siarhei Filinski <sfilisnky@gmail.com>
2023-11-13 15:42:49 +0100

    Ejected `auth` module.

---
+++ packages/teammate-search/auth/config/webpack.microservice.config.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common.js');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './bootstrap': './src/bootstrap'
+            },
+            shared: packageJson.dependencies
+        }),
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
